361d89ed9620367b8dcf1deddc3ef94bcdcae643
mariuszpozarlik <pozarek89@gmail.com>
2022-06-14 14:48:16 +0200

    changed to non blocking mode and client to OOP

--- client.py
+++ client.py
@@ -1,58 +1,77 @@
 import socket
 import time
 
-HEADER = 64
-PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-suffix = 3
-SERVER = f"192.168.168.4"
-ADDR = (SERVER, PORT)
-result = True
-
-global client
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-while(result):
-    try:
-        print(f"Attepmt to connect address {SERVER}")
-        client.connect(ADDR)
-        result = False
-    except Exception as e:
-        print(e.__class__)
-
-def send(msg):
-    global client
-    try:
-        message = msg.encode(FORMAT)
-        msg_length = len(message)
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b' ' * (HEADER - len(send_length))
-        client.send(send_length)
-        client.send(message)
-        #time.sleep(1)
-        print(client.recv(2048).decode(FORMAT))
-    except Exception as e:
-        print(e.__class__)
-        del client
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = True
-        while (result):
+class EZclient:
+    def __init__(self, server_IP, server_PORT = 5050, server_HEADER = 64, server_FORMAT = 'utf-8'):
+        self.HEADER = server_HEADER
+        self.PORT = server_PORT
+        self.FORMAT = server_FORMAT
+        self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.SERVER = server_IP
+        self.ADDR = (self.SERVER, self.PORT)          
+        self.TIMEOUT = 3
+        self.BLOCKING_MODE = False
+        
+    def connectToServer(self):
+        self.result = True
+        while(self.result):
             try:
-                print(f"Attepmt to re-connect address {SERVER}")
-                client.connect(ADDR)
-                result = False
+                print(f"Attepmt to connect address {self.SERVER}")
+                self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.client.setblocking(self.BLOCKING_MODE)
+                self.client.settimeout(self.TIMEOUT)
+                #time.sleep(1)
+                self.client.connect(self.ADDR)
+                self.result = False
+            except ConnectionRefusedError as e:
+                print(e.__class__)
             except Exception as e:
                 print(e.__class__)
+        return True
+    def sendMessage(self, msg):
+        try:
+            message = msg.encode(self.FORMAT)
+            msg_length = len(message)
+            send_length = str(msg_length).encode(self.FORMAT)
+            send_length += b' ' * (self.HEADER - len(send_length))
+            self.client.send(send_length)
+            self.client.send(message)
+            #time.sleep(1)
+            return self.client.recv(2048).decode(self.FORMAT)
+        except ConnectionResetError as e:
+            print(e.__class__)
+            self.connectToServer()
+        except ConnectionAbortedError as e:
+            print(e.__class__)
+            self.connectToServer()
+        except socket.timeout as e:
+            print(e.__class__)
 
-cnt = 0
-while(True):
-    send("very long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent withoutchange of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLA")
 
-    send("shutdown -s -t 10")
+if __name__ == "__main__":
+    client = EZclient(server_IP = f"192.168.168.3")    
+    cnt = 0
+    if(client.connectToServer()):
+        pass
+    while(True):
+        res = client.sendMessage(input())
+        print(res)
+        
+#         client.sendMessage("very long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent withoutchange of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLAvery long text to sent without any change of whatever I'm only writing without thinking bla blA BLA")
+#      
+#         client.sendMessage("shutdown -s -t 10")
+#      
+#         client.sendMessage(f"Hello test server!")
+#  
+#         client.sendMessage(str(cnt))
+        
+#         try:
+#             print(client.client.recv(2048).decode('utf-8'))
+#         except:
+#             pass
+#         print("asd")
+#         cnt+=1
+#         if cnt > 20: 
+#             break
 
-    send(f"Hello test server!")
-
-    send(str(cnt))
-    cnt+=1
-
-send(DISCONNECT_MESSAGE)
+client.sendMessage(client.DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -2,51 +2,57 @@ import socket
 import threading
 
 class EZServer:
-    def __init__(self):
-        self.HEADER = 64
-        self.PORT = 5050
+    def __init__(self, server_HEADER = 64, server_PORT = 5050):
+        self.HEADER = server_HEADER
+        self.PORT = server_PORT
         self.SERVER = socket.gethostbyname(socket.gethostname())
         self.ADDR = (self.SERVER, self.PORT)
         self.FORMAT = 'utf-8'
         self.DISCONNECT_MESSAGE = "!DISCONNECT"
+        self.BLOCKING_MODE = False
+        self.TIMEOUT = 5
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind(self.ADDR)
+        self.server.setblocking(self.BLOCKING_MODE)
+        self.server.settimeout(self.TIMEOUT)
         self.msg = ''
 
     def handle_client(self, conn, addr):
         print(f"[NEW CONNECTION] {addr} connected.")
         connected = True
         while connected:
-            try:
+            try:                
                 msg_length = conn.recv(self.HEADER).decode(self.FORMAT)
                 if msg_length:
-                    #print(f"[MESSAGE SIZE] -> {msg_length}")
+                    print(f"[MESSAGE SIZE] -> {msg_length}")
                     msg_length = int(msg_length)
                     self.msg = conn.recv(msg_length).decode(self.FORMAT)
                     if self.msg == self.DISCONNECT_MESSAGE:
-                        connected = False
-                    #print(f"[INCOMMING MESSAGE FROM] {addr} -> {self.msg}")
+                        conn.send("[DISCONNECTED FROM SERVER]".encode(self.FORMAT))
+                        connected = False                        
+                    print(f"[INCOMMING MESSAGE FROM] {addr} -> {self.msg}")
 
                     # process message
 
                     conn.send(f"[RESPONSE FROM SERVER]-> message <<{self.msg}>> processed with status {0}".encode(self.FORMAT))
             except ConnectionResetError as exc:
                 connected = False
-                print(exc)
+                print(exc.__class__)
             except Exception as e:
                 print(e.__class__)
-
         conn.close()
 
     def start(self):
         self.server.listen()
         print(f"[LISTENING] Server is listening on {self.SERVER}")
         while True:
-            conn, addr = self.server.accept()
-            thread = threading.Thread(target=self.handle_client, args=(conn, addr))
-            thread.start()
-            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
-
+            try:
+                conn, addr = self.server.accept()
+                thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+                thread.start()
+            except Exception as e: 
+                pass
+            print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")      
 
 if __name__ == "__main__":
     s = EZServer()
